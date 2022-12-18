f851e3719749dbae9d18bd12e8bb876fbee09357
MitsukiIwamura <boc.fsfb@gmail.com>
2022-12-18 16:04:58 +0900

    markdown

--- src/components/VisibleTest.tsx
+++ src/components/VisibleTest.tsx
@@ -1,10 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect }from "react";
 
-export const Counter = () => {
+export const VisibleTest = () => {
   const [count, setCount] = useState<number>(0);
   const handleClickCount = (): void => {
     setCount((prev) => (prev += 1));
   };
 
+  useEffect(() => {
+    console.log('hello');
+    
+    setCount(1)
+  }, [])
   return <button onClick={handleClickCount}>{count}</button>;
 };
